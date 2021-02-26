71beb871d0d68de732d99bdaab18742f725f602d
PurabPoddar <79688018+PurabPoddar@users.noreply.github.com>
2021-02-26 12:57:24 +0530

    Initial Commit

---
+++ js/logic.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",(()=>{new Glide(".slideshow",{autoplay:3750,rewindDuration:500,animationDuration:500}).mount(),new Glide(".carousel",{perView:5,breakpoints:{640:{perView:1}}}).mount()}));
